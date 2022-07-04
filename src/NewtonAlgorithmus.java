c7c05c28cf7c9adab4458154af4787addafe677b
duluk <96245211+duluk-create@users.noreply.github.com>
2022-07-04 20:40:09 +0200

    Approximation der Quadratwurzel

---
+++ src/NewtonAlgorithmus.java
@@ -0,0 +1,17 @@
+public class NewtonAlgorithmus {
+    public static void newtonWurzel(double n, double q){
+        if(Math.abs(n-(q*q)) < n*0.000000001){
+            System.out.println(q);
+        }
+        else{
+            if(q*q == n){
+                System.out.println(q);
+            }
+            if(q*q < n || q*q > n){
+                q = (q + (n/q))/2;
+                newtonWurzel(n, q);
+            }
+        }
+
+    }
+}
